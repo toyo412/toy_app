65ba34759f40fd8c46306e28c9892360d0aab267
toyo412 <ttttytt412@gmail.com>
2023-08-29 17:39:29 +0900

    / index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  def hello
+  def index
     render html: "hello, world!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root 'application#index'
   # Defines the root path route ("/")
   # root "articles#index"
+  
 end
